f5d94529ccea2557ea998bb6f8fa4c3e3d90b8cb
Daniel Müller <daniel.daniel@gmx.ch>
2021-10-13 18:07:15 +0200

    Add ball tracking sample scene

---
+++ Assets/VROOM/Runtime/Scripts/MovingAverage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace VROOM.Runtime.Scripts
+{
+    public class MovingAverage  
+    {
+        public float Average { get; private set; }
+
+        public void Add(float newSample)
+        {
+            sampleAccumulator += newSample;
+            samples.Enqueue(newSample);
+
+            if (samples.Count > windowSize)
+                sampleAccumulator -= samples.Dequeue();
+
+            Average = sampleAccumulator / samples.Count;
+        }
+
+        float sampleAccumulator;
+        int windowSize = 5;
+        Queue<float> samples = new Queue<float>();
+    }
+}
---
+++ Assets/VROOM/Runtime/Scripts/PassDetection.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using Scripts;
+using UnityEngine;
+
+namespace VROOM.Runtime.Scripts
+{
+    public class PassDetection : MonoBehaviour
+    {
+        [Header("Prefabs")] 
+        [SerializeField] GameObject _soccerBall;
+
+        [Header("Tracked Objects")] 
+        [SerializeField] ViveMotionTracker _tracker;
+        [SerializeField] Transform _foot;
+        [SerializeField] Transform _passTarget;
+
+        void OnEnable() => SpaceCalibration.onCalibrationStart += OnCalibrationStarted;
+
+        void OnCalibrationStarted() => _calibrationOffset = _foot.transform.position;
+
+        void Start() => _movingAverage = new MovingAverage();
+    
+        void Update() => _foot.transform.position = _tracker.GetPosition() - _calibrationOffset;
+        
+        void FixedUpdate() => DetectKick();
+
+        void DetectKick()
+        {
+            _footPosition = _foot.transform.position;
+            var deltaFootPosition = _footPosition - _previousFootPosition;
+
+            var passTargetPosition = _passTarget.transform.position;
+            _passTargetDirection = passTargetPosition - deltaFootPosition;
+
+            var footMovementInTargetDirection = Vector3.Project(deltaFootPosition, _passTargetDirection).z;
+
+            _movingAverage.Add(footMovementInTargetDirection);
+
+            var velocityMeterPerSecond = _movingAverage.Average / Time.fixedDeltaTime;
+            Debug.Log(velocityMeterPerSecond);
+
+
+            if (velocityMeterPerSecond > 1 && _isReadyForPass)
+            {
+                print("Kick");
+                var currentBall = Instantiate(_soccerBall, _footPosition, Quaternion.identity);
+                var rigidBody = currentBall.GetComponent<Rigidbody>();
+                rigidBody.AddRelativeForce(deltaFootPosition / Time.fixedDeltaTime, ForceMode.VelocityChange);
+                _isReadyForPass = false;
+                StartCoroutine(ResetPassAfter(currentBall, 3f));
+            }
+
+            _previousFootPosition = _footPosition;
+        }
+
+        IEnumerator ResetPassAfter(GameObject objectToDestroy, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+        
+            Destroy(objectToDestroy);
+            _isReadyForPass = true;
+        }
+        void OnDrawGizmos()
+        {
+            Gizmos.DrawWireMesh(
+            _passTarget.GetComponent<MeshFilter>().sharedMesh,
+            _passTarget.transform.position,
+            _passTarget.transform.rotation,
+            _passTarget.transform.localScale);
+        }
+
+        Vector3 _passTargetDirection;
+        Vector3 _previousFootPosition;
+        Vector3 _footPosition;
+        MovingAverage _movingAverage;
+        bool _isReadyForPass = true;
+
+        Vector3 _calibrationOffset;
+    }
+    
+    
+}
