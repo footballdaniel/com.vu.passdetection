f26fd0575370576d158fbac4189337ccda5f6962
Daniel Müller <daniel.daniel@gmx.ch>
2024-07-19 13:31:33 +0200

    refactor naming

--- Runtime/Scripts/ViveMotionTracker.cs
+++ Runtime/Scripts/ViveMotionTracker.cs
@@ -1,24 +1,12 @@
 ﻿using System;
 using UnityEngine;
-using Valve.VR;
 
 namespace Balltracking.Scripts
 {
     [Serializable]
-    public class ViveMotionTracker : MonoBehaviour 
+    public class ViveMotionTracker : MonoBehaviour
     {
-        public Vector3 Position =>  _trackedObject.transform.position;
+        public Vector3 Position => transform.position;
         
-        [SerializeField] SteamVR_TrackedObject.EIndex _device = SteamVR_TrackedObject.EIndex.Device1;
-
-        void Awake() => InstantiateTracker();
-
-        void InstantiateTracker()
-        {
-            _trackedObject = gameObject.AddComponent<SteamVR_TrackedObject>();
-            _trackedObject.index = _device;
-        }
-
-        SteamVR_TrackedObject _trackedObject;
     }
 }
